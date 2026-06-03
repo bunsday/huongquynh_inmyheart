78e7bde9f980c09c469d656215cc3b96ca3054e1
bunsday <129835187+bunsday@users.noreply.github.com>
2026-06-04 00:13:35 +0700

    Update configuration strings in config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "Gửi người đã phát hiện ra sự theo đuổi của tớ",
+    introTitle: 'Crush yêu dấuuuuu, 55kg cụa toiii',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: ' Bằng tất cả sự chân thành tớ muốn khởi động một buổi đi chơi vào ngày thứ 7 tuần này 🥰',
+    desc: 'Thoát web là chấp nhận lời mời của tớ rồi nhé!!!',
+    btnYes: 'Thích lắmmmm <33',
+    btnNo: 'Không à nha :3',
+    question: 'Trên thế  <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
+    messLink: 'https://www.facebook.com/nqtruong2107' 
 }
