6abcc01805e1b39ee6af025b00e0d5a1aa4c5250
bunsday <129835187+bunsday@users.noreply.github.com>
2026-06-04 21:36:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Gửi người đã phát hiện ra sự theo đuổi của tớ",
-    introTitle: 'Crush yêu dấuuuuu, 55kg cụa toiii',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Dứa_Xinh_Đẹp_Thông_Minh,",
+    introTitle: 'Cho cô nàng Cute nhất thế giới aka Dứa ~ 55kg chưa là cụa toiii',
+    introDesc: `Nếu như gặp gỡ đã là duyên rồi thì tớ muốn đưa mối lương duyên này đi thật xa hơn nữaaaaaa`,
     btnIntro: '^^HiHi^^',
     title: ' Bằng tất cả sự chân thành tớ muốn khởi động một buổi đi chơi vào ngày thứ 7 tuần này 🥰',
     desc: 'Thoát web là chấp nhận lời mời của tớ rồi nhé!!!',
     btnYes: 'Thích lắmmmm <33',
     btnNo: 'Không à nha :3',
-    question: 'Trên thế  <3',
+    question: 'Sao cậu lại đồng ý đi chơi với tớ thế hí hí <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Tại cậuuuu ấyyyyy<33333333',
+    mess: 'Tớ biết mà 🥰.Thật là có mắt nhìn người quá điiii 😘😘',
+    messDesc: 'Thứ 7 này, tớ qua đón đi chơi nhaaaa',
+    btnAccept: 'Được gòiiiii <3',
     messLink: 'https://www.facebook.com/nqtruong2107' 
 }
